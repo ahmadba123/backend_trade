1f0d21eb0f105dff299fc21b657180bab7d74647
ahmad badawi <ahmad.badawu12@gmail.com>
2022-09-25 14:49:52 +0300

    update

--- controller/trade.js
+++ controller/trade.js
@@ -4,9 +4,9 @@ const db = require("../database_Connection/config");
 //this function to get data for trade  from data base
 exports.getAllTrade = async (req, res, next) => {
   try {
-    const [Trades, _] = await Trade.findAll(req.query.limit, req.query.offset);
-    const count =await Trade.Count()
-    res.status(200).send({ count: Trades.length, Trades ,countTrade:count[0]});
+    const [Trades, _] = await Trade.findAll(req.query.limit, req.query.offset, req.query.query);
+    const count = await Trade.Count()
+    res.status(200).send({ count: Trades.length, Trades, countTrade: count[0] });
 
   } catch (error) {
     next(error);
@@ -36,7 +36,7 @@ exports.createNewTrade = async (req, res, next) => {
     let myQuery = `INSERT INTO mt5_deals (Deal,Login,Action,Entry,Time,Symbol,Price,Profit,Volume)
     VALUES('${Deal}','${Login}','${Action}','${Entry}','${Time}','${Symbol}','${Price}','${Profit}','${Volume}')`;
     db.execute(myQuery).then(() =>
-      res.status(200).json({ success: true, message: "Trade Added !"  })
+      res.status(200).json({ success: true, message: "Trade Added !" })
     );
   }
 
--- model/trade.js
+++ model/trade.js
@@ -12,15 +12,24 @@ class Trade {
         this.Profit = Profit;
         this.Volume = Volume;
     }
-//this function to get and  divide data 10 record in one page from data base
-    static async findAll(limit, offset) {
-        let sql = "SELECT * FROM mt5_deals LIMIT ?, ?";
+    //this function to get and  divide data 10 record in one page from data base
+    static async findAll(limit, offset, query) {
+        let sql = "SELECT * FROM mt5_deals where Deal like ? OR Login Like ?  order by Deal ASC LIMIT ?, ?";
+        query = query ? query : "";
+
         limit = Number(limit) >= 0 ? Number(limit) : 10;
         offset = Number(offset) >= 0 ? Number(offset) : 0;
-        let results = (await db.query(sql, [offset, limit])).values();
+
+        let results = (await db.query(sql, [query + "%", query + "%", offset, limit])).values();
         return results;
     }
-//this function count all data  from data base
+
+    static async findAllTrade() {
+        let sql = "SELECT * FROM mt5_deals";
+        let results = (await db.query(sql));
+        return results;
+    }
+    //this function count all data  from data base
     static async Count() {
         let sql = "SELECT  COUNT(Deal) FROM mt5_deals  ";
 
@@ -28,6 +37,7 @@ class Trade {
         // console.log(await db.query(sql))
         return results;
     }
+
 }
 
 module.exports = Trade;
